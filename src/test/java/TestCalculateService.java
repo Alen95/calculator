9ee2399f543941ff5762973ade58269e12fbcfe7
Jahic Alen <jahicalen@hotmail.fr>
2020-10-30 14:40:46 +0100

    Updated proj

---
+++ src/test/java/TestCalculateService.java
@@ -0,0 +1,15 @@
+
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.Test;import org.junit.runner.RunWith;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+public class TestCalculateService {
+
+	@Test
+	public void testAddition() {
+		assertTrue(false);
+	}
+}
