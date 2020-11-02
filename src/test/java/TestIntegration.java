77879276d7fe094f2ac314a7d483ea6ca42048bc
Jahic Alen <jahicalen@hotmail.fr>
2020-11-02 21:58:38 +0100

    Update project

--- src/test/java/TestIntegration.java
+++ src/test/java/TestIntegration.java
@@ -3,12 +3,10 @@
 import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;import org.junit.runner.RunWith;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import com.example.model.OperationModel;
 import com.example.service.CalculateSimple;
 
-@RunWith(SpringJUnit4ClassRunner.class)
 public class TestIntegration {
 
 	@Test
--- src/test/java/TestUnit.java
+++ src/test/java/TestUnit.java
@@ -3,11 +3,9 @@
 import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;import org.junit.runner.RunWith;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import com.example.model.OperationModel;
 
-@RunWith(SpringJUnit4ClassRunner.class)
 public class TestUnit {
 
 	/**
